4c51629e227e4910acf85ccbb78c9eb8fa6ff0e6
Yeonsu1027 <rito1205@naver.com>
2024-03-08 10:50:57 +0900

    react

--- nextjs-book-102/src/comps/BookMain.js
+++ nextjs-book-102/src/comps/BookMain.js
@@ -9,17 +9,21 @@ const BookMain = () => {
   const [search, setSearch] = useState("");
   const [isbn, setIsbn] = useState("");
   return (
-    <section className="book main">
-      <BookSearch search={search} setSearch={setSearch} />
-      <article className="book body">
-        <aside className="book list">
-          <BookList search={search} setIsbn={setIsbn} />
-        </aside>
-        <aside className="book detail">
-          <BookDetail isbn={isbn} />
-        </aside>
-      </article>
-    </section>
+    <article className="book main">
+      <aside className="book list">
+        <BookSearch search={search} setSearch={setSearch} />
+        <BookList search={search} setIsbn={setIsbn} />
+      </aside>
+      <aside className="book detail">
+        {/*
+         조건부 Rendering
+        isbn state 값이 "" 또는 null 등이 아니면 BookDetail 을 화면에 표현하라
+        */}
+        {isbn && <BookDetail isbn={isbn} />}
+      </aside>
+    </article>
   );
 };
+// {isbn && <BookDetail isbn={isbn} />}  isbn 값이 있을때만 디테일 화면보여라
+// if 1 : "" ~문대신 && 로표시 가능
 export default BookMain;
